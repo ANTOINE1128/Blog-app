043f4c81c0c3e637ad6a4351f147fcbd2bdc8c99
ANTOINE1128 <makdessyantoine@gmail.com>
2023-09-02 00:06:14 +0300

    fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,6 @@ class PostsController < ApplicationController
     @post = Post.new
     @user = User.find(params[:user_id])
     @posts = @user.posts.order(created_at: :asc)
-    
   end
 
   def show
