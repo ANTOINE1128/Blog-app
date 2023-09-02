3c4c17b36de751d7db8863a209d32039e8f9e492
ANTOINE1128 <makdessyantoine@gmail.com>
2023-09-02 21:37:18 +0300

    fix rubocop linters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,16 +5,14 @@ class LikesController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
     @like = Like.new(author_id: @user.id, post_id: @post.id)
-
     if @like.save
       flash[:notice] = 'Like was successfully created.'
     else
       flash[:alert] = 'Failed to add like.'
     end
-
     redirect_to user_post_path(@user, @post)
   end
   def like_params
-    params.require(:like).permit(:author_id)  # Make sure you have :author_id in the permit list.
+    params.require(:like).permit(:author_id) # Make sure you have :author_id in the permit list.
   end
 end
--- app/models/renamelike.rb
+++
@@ -1,5 +0,0 @@
-class RenameLikes
-    def change
-        rename_column :your_table_name, :old_column_name, :new_column_name
-      end
-end
