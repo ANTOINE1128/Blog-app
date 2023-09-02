7937962078c880d681f5df1c620e77b762463025
ANTOINE1128 <makdessyantoine@gmail.com>
2023-09-02 21:42:16 +0300

    fix linters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -12,7 +12,7 @@ class LikesController < ApplicationController
     end
     redirect_to user_post_path(@user, @post)
   end
-  
+
   def like_params
     params.require(:like).permit(:author_id) # Make sure you have :author_id in the permit list.
   end
