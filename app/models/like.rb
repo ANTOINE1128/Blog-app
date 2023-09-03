fc9f4392e9f3b9fb1a824da31658f697b1626c30
ANTOINE1128 <makdessyantoine@gmail.com>
2023-09-03 12:04:22 +0300

    fix rubocop linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-  belongs_to :user , foreign_key: 'author_id'
+  belongs_to :user, foreign_key: 'author_id'
   belongs_to :post
 
   after_save :update_likes_counter_for_a_post
