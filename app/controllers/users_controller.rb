71eea57239f4e20956683d46d83924077ad48eac
ANTOINE1128 <makdessyantoine@gmail.com>
2023-09-09 23:34:25 +0300

    add device

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    if params[:id] == 'sign_out'
+      sign_out(current_user)
+      redirect_to new_user_session_path, notice: 'You have signed out.'
+    else
+      @user = User.find(params[:id])
+      @user_posts = @user.posts
+      @recent_posts = @user_posts.limit(3)
+    end
   end
 end
